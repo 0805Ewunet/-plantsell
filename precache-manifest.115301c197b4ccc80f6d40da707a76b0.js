470ea6da535f714a1679b145567f833a826f9888
Ewunet Kassa <ewunetkassa@gmail.com>
2020-11-21 19:35:31 -0500

    Updates

---
+++ precache-manifest.115301c197b4ccc80f6d40da707a76b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f713745b050cd141b44b30ccbd762aa",
+    "url": "/0805Ewunet/-plantsell.git/index.html"
+  },
+  {
+    "revision": "d3ce65104b376401566c",
+    "url": "/0805Ewunet/-plantsell.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "81e07c4eb57d66368c2a",
+    "url": "/0805Ewunet/-plantsell.git/static/css/main.e6f9fca1.chunk.css"
+  },
+  {
+    "revision": "d3ce65104b376401566c",
+    "url": "/0805Ewunet/-plantsell.git/static/js/2.2663192f.chunk.js"
+  },
+  {
+    "revision": "b150fcc69cc9891f4d1afdfc42d99191",
+    "url": "/0805Ewunet/-plantsell.git/static/js/2.2663192f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e07c4eb57d66368c2a",
+    "url": "/0805Ewunet/-plantsell.git/static/js/main.4a2629d1.chunk.js"
+  },
+  {
+    "revision": "6d276ecba8851db9f5ea",
+    "url": "/0805Ewunet/-plantsell.git/static/js/runtime-main.7175e5bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/0805Ewunet/-plantsell.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
